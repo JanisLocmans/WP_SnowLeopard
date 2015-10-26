a4d1edc2eeb5ebcde0caf804cc449eb55bf7bcec
John <J.Locmans@gmail.com>
2015-10-26 14:13:01 +0200

    update:First Initilization !

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<!--HEADER FILE -->
+		<header class="site-header">
+				<div class="site-header-wrap">
+						<H1>HOME</H1>
+				</div>
+
+						
+				<div class="site-logo">
+						<a href="index.php" title="snowleopard" rel="home">
+							<div class="site-logo-wrapper">
+								<img src="pic/logo.jpg">
+							</div>
+						</a>
+				</div>
+
+				<div class="side-border">
+					<div class="border-vertical-line">
+
+					</div>
+						<a id="src-button" class="side-button"><img src="pic/src.png"></a>
+						<a id="side-bar-button" class="side-button"><img src="pic/arw.png"></a>
+						<a id="nav-button" class="side-button"><img src="pic/nav.png"></a>
+				</div>	
+
+				<!--<nav class="site-main-nav">
+					<ul>
+						<li onclick="color(this)">Home</li>
+						<li>About</li>
+						<li>Contact</li>
+						<li>Portfolio</li>
+					</ul>
+				</nav>-->
+		</header>
+		<!--HEADER FILE End-->
+		<!--SIDEBAR FILE START-->
+			<div id="js-sidebar" class="sidebar-wrapper">
+			</div>
+		<!--SIDEBAR FILE End-->
+		<div class="content">
+				<div class="container">
+						<div class="page">
+							<div class="main">
+								<div class="timeline">
+								</div>
+									<div class="blog-post-container">		
+										<article class="blog-post">
+											<a>
+												<div class="featured-img">
+													<!--<div class="blog-button-left">
+														<img src="pic/arrow-left.png">
+													</div>
+													<div class="blog-button-right">
+														<img src="pic/arrow-right.png">
+													</div>-->
+												</div>
+											</a>
+											<div class="blog-post-inner">
+												<header class="blog-post-header">
+													<h1 class="blog-post-title">
+														Snow Leopard in Wild
+													</h1>
+													<aside class="blog-post-meta">
+														<span class="tm">Date:2015.10.02</span>
+														|
+														<span class="usr">By:FrostFurry</span>
+														|
+														<span class="cat">Tags:Feline</span>
+														|
+														<span class="cmnt">0 comment</span>
+														|
+													</aside>
+												</header>
+												<div class="blog-post-content">
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+															cillum dolore eu fugiat nulla pariatur. Excepteur 
+															sint occaecat cupidatat non proident, sunt in culpa
+															 qui officia deserunt mollit anim id est laboru</p>
+														<aside class="blog-read-more"> READ MORE </aside>
+
+												</div>
+											</div>							
+										</article>
+
+										<article class="blog-post">
+											<a>
+												<div class="featured-img2">
+													<!--<div class="blog-button-left">
+														<img src="pic/arrow-left.png">
+													</div>
+													<div class="blog-button-right">
+														<img src="pic/arrow-right.png">
+													</div>-->
+												</div>
+											</a>
+											<div class="blog-post-inner">
+												<header class="blog-post-header">
+													<h1 class="blog-post-title">
+														Snow Leopard in Wild
+													</h1>
+													<aside class="blog-post-meta">
+														<span class="tm">Date:2015.10.02</span>
+														|
+														<span class="usr">By:FrostFurry</span>
+														|
+														<span class="cat">Tags:Feline</span>
+														|
+														<span class="cmnt">0 comment</span>
+														|
+													</aside>
+												</header>
+												<div class="blog-post-content">
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+															cillum dolore eu fugiat nulla pariatur. Excepteur 
+															sint occaecat cupidatat non proident, sunt in culpa
+															 qui officia deserunt mollit anim id est laboru</p>
+														<aside class="blog-read-more"> READ MORE </aside>
+
+												</div>
+											</div>									
+										</article>
+
+									</div>
+							</div>
+						</div>
+						<div class="page1">
+
+						</div>
+				</div>
+		</div>
+	</body>
+</html>
