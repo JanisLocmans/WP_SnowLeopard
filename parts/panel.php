c93ca5e390a607d4036a115f6c60eba08a975b7f
john <j_locmans@gmail.com>
2016-08-30 04:45:26 +0300

    Update:Overhaul of the Panel

--- parts/panel.php
+++ parts/panel.php
@@ -1,29 +1,25 @@
-<header class="site-header">
-
-				<div id="site-header-id" class="site-header-wrap">					
-
+<header class="snow_frame">
+				<div class="top-frame">
+					<div id="site-header-id" class="menu-buttons">					
 						<a id="src-button" class="side-button"><img src="<?php bloginfo('template_directory'); ?>/pic/src.png"></a>
 						<a id="side-bar-button" class="side-button"><img src="<?php bloginfo('template_directory'); ?>/pic/arw.png"></a>
 						<a id="nav-button" class="side-button"><img src="<?php bloginfo('template_directory'); ?>/pic/nav.png"></a>
-				</div>
+					</div>
 
-				<div id="js-sidebar" class="sidebar-wrapper">
-					<ul>
-						<li><a>Home</a></li>
-						<li><a>Menu</a></li>
-						<li><a><img src="<?php bloginfo('template_directory'); ?>/pic/logo.png"></a></li>
-						<li><a>Archive</a></li>
-						<li><a>BookShelf</a></li>											
-					</ul>
-				</div>
-
-				<div id="panel-right">
-					<div id="panel-button-right">
+					<div id="js-sidebar" class="menu-list">
+							<li><a>Home</a></li>
+							<li><a>Menu</a></li>
+							<li class="logo"><a><img src="<?php bloginfo('template_directory'); ?>/pic/logo.png"></a></li>
+							<li><a>Archive</a></li>
+							<li><a>BookShelf</a></li>											
 					</div>
 				</div>
-				<div id="panel-bottom">
-					<div id="panel-button-bottom">
-					</div>
+				<div class="right-frame">
+				</div>
+				<div class="bottom-frame">
+				</div>
+				<div class="left-frame">
 				</div>
 
 </header>
+
